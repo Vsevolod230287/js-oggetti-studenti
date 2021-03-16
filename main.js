5a1344c07f7a9b0942996aea19c65aa7a715256d
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-16 16:37:08 +0100

    Stampato anche tutte le proprietà di tutti studenti

--- main.js
+++ main.js
@@ -4,8 +4,10 @@
 // tutte le proprietà dell'oggetto.
 
 //   Creare un array di oggetti di studenti.
- // Ciclare su tutti gli studenti e stampare
+
+// Ciclare su tutti gli studenti e stampare
 //    per ognuno di essi, nome e cognome.
+
 // Dare la possibilità all’utente,
 //     attraverso 3 prompt(),
 //  di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
@@ -21,5 +23,34 @@ var cognome = $('#cognome');
 var anni = $('#anni');
 
 for (var key in studente) {
-$('.nome').append(" " + studente[key]);
+  $('.studente').append(" " + studente[key]);
+}
+// ----------------------------------------------------
+var arrayOggetti = [
+  {
+    nome: "Vsevolod",
+    cognome: "Cerneavschi",
+    anni: 34,
+
+  },
+  {
+    nome: "Ernesto",
+    cognome: "Cighevara",
+    anni: 36,
+  },
+  {
+    nome: "Bernardo",
+    cognome: "Stolnici",
+    anni: 35,
+  }
+]
+
+var divArrayOgetti = $('.array-oggetti');
+
+for (var i = 0; i < arrayOggetti.length; i++) {
+  for (var key in arrayOggetti[i]) {
+    console.log(arrayOggetti[i][key]);
+     divArrayOgetti.append("</div>" + arrayOggetti[i][key] + "<div>")
+
+  }
 }
