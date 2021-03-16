df4ff3df00c39e89e1dcd2a781d65c9b25918f4e
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-16 15:43:44 +0100

    Stapato a schermo oggetto studente!

---
+++ main.js
@@ -0,0 +1,25 @@
+// Creare un oggetto che descriva uno studente,
+// con le seguenti proprietà: nome, cognome e età.
+//  Stampare a schermo attraverso un ciclo for-in
+// tutte le proprietà dell'oggetto.
+
+//   Creare un array di oggetti di studenti.
+ // Ciclare su tutti gli studenti e stampare
+//    per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente,
+//     attraverso 3 prompt(),
+//  di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+var studente = {
+  nome: "Vsevolod",
+  cognome: "Cerneavschi",
+  anni: 34,
+}
+
+var nome = $('#nome');
+var cognome = $('#cognome');
+var anni = $('#anni');
+
+for (var key in studente) {
+$('.nome').append(" " + studente[key]);
+}
